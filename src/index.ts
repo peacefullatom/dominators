8f92718763889dc957fa3ac526dd718c3abce706
Yuriy Markov <peacefullatom@gmail.com>
2020-02-15 03:19:10 +0500

    increased number of planetary systems and added stats

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import Galaxy from './galaxy/galaxy';
+import { TGalaxy } from './galaxy/galaxy.types';
 
-const galaxy = Galaxy();
+const start = new Date();
+new Promise<TGalaxy>(resolve => {
+  const galaxy = Galaxy();
+  resolve(galaxy);
+}).then(galaxy => {
+  const end = new Date();
+  const planets = galaxy.planetarySystems
+    .map(s => s.planets.length)
+    .reduce((total, num) => total + num);
+  const portals = galaxy.planetarySystems
+    .map(s => s.portals.length)
+    .reduce((total, num) => total + num);
 
-const app = document.getElementById('app');
+  const app = document.getElementById('app');
 
-if (app) {
-  app.innerHTML = `Galaxy v 1.0.0<br>`;
-  app.innerHTML +=
-    `<pre><code>` + JSON.stringify(galaxy, null, 2) + `</code></pre>`;
-}
+  if (app) {
+    app.innerHTML = `Galaxy v 1.0.0<br>`;
+    app.innerHTML += `Generation time: ${end.getTime() -
+      start.getTime()}ms<br>`;
+    app.innerHTML += `Planetary systems: ${galaxy.planetarySystems.length}<br>`;
+    app.innerHTML += `Planets: ${planets}<br>`;
+    app.innerHTML += `Portals: ${portals}<br>`;
+  }
+});
