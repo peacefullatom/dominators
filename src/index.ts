1acb02713d94715cc24ad7c9aab8ed6fa05d4713
Yuriy Markov <peacefullatom@gmail.com>
2020-02-15 03:00:26 +0500

    added output of generated galaxy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import Galaxy from './galaxy/galaxy';
+
+const galaxy = Galaxy();
+
 const app = document.getElementById('app');
 
 if (app) {
-  app.innerHTML = `Galaxy v 1.0.0`;
+  app.innerHTML = `Galaxy v 1.0.0<br>`;
+  app.innerHTML +=
+    `<pre><code>` + JSON.stringify(galaxy, null, 2) + `</code></pre>`;
 }
