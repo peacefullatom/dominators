1531ac8a202ee66e4d111433d7e63f363e7a54cc
Yuriy Markov <peacefullatom@gmail.com>
2020-02-27 13:18:09 +0500

    added basic controls for testing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,61 @@
 import Galaxy from './galaxy/galaxy';
+import {
+  galaxyDensities,
+  galaxyDensityDense,
+  galaxyDensityMedium,
+  galaxyDensitySparse,
+  galaxyDensityVeryDense,
+  galaxyDensityVerySparse,
+} from './galaxy/galaxy.const';
 
-const start = new Date().getTime();
-(document as any).g = new Galaxy();
-console.log(`generation time: ${new Date().getTime() - start}`);
+const densityLabel = (d: number): string => {
+  if (d === galaxyDensityVeryDense) {
+    return 'very dense';
+  }
+  if (d === galaxyDensityDense) {
+    return 'dense';
+  }
+  if (d === galaxyDensityMedium) {
+    return 'medium';
+  }
+  if (d === galaxyDensitySparse) {
+    return 'sparse';
+  }
+  if (d === galaxyDensityVerySparse) {
+    return 'very sparse';
+  }
+  return '';
+};
 
 const app = document.getElementById('app');
 if (app) {
-  app.innerHTML = 'q';
-  app.innerHTML += `<pre>${JSON.stringify((document as any).g, null, 2)}</pre>`;
+  const start = new Date().getTime();
+  const galaxy = new Galaxy({ parent: app });
+  (document as any).g = galaxy;
+  console.log(`generation time: ${new Date().getTime() - start}`);
+
+  galaxy.show();
+
+  const generate = document.createElement('button');
+  generate.addEventListener('click', () => {
+    const systems = galaxy.generateSystems();
+    galaxy.canvas.showSystems(systems);
+    galaxy.canvas.showWormholes(systems);
+  });
+  generate.innerText = 'Generate';
+  app.appendChild(generate);
+
+  const density = document.createElement('select');
+  galaxyDensities.forEach(d => {
+    const option = document.createElement('option');
+    option.value = d.toString();
+    option.selected = galaxy.density === d;
+    option.text = densityLabel(d);
+    density.appendChild(option);
+  });
+  density.addEventListener('change', v => {
+    const value = (v?.target as HTMLSelectElement)?.value;
+    galaxy.setDensity(parseInt(value));
+  });
+  app.appendChild(density);
 }
