0c1bb776ca41956ac9e33a7063b1bdfc16b8a866
Yuriy Markov <peacefullatom@gmail.com>
2020-03-13 15:12:30 +0500

    added keyboard support for galaxy settings

--- src/game/menu/start/start-layout/StartLayout.tsx
+++ src/game/menu/start/start-layout/StartLayout.tsx
@@ -2,12 +2,30 @@ import './StartLayout.scss';
 
 import { faAngleDoubleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import MenuLayout from '../../menu-layout/MenuLayout';
 import { TStartLayout } from './StartLayout.types';
 
 const StartLayout: React.FC<TStartLayout> = ({ children, back, forward }) => {
+  const keyboard = (event: KeyboardEvent): void => {
+    const { keyCode } = event;
+    // n
+    if (keyCode === 78 && typeof forward === 'function') {
+      forward();
+    }
+    // b
+    if (keyCode === 66 && typeof back === 'function') {
+      back();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyboard);
+
+    return (): void => document.removeEventListener('keydown', keyboard);
+  });
+
   return (
     <MenuLayout>
       <div className='start_layout'>
