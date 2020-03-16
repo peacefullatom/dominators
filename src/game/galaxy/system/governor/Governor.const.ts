cd643afebe2657bd5188cb02417c0ea31f78936e
Yuriy Markov <peacefullatom@gmail.com>
2020-03-16 16:20:05 +0500

    updated governor

---
+++ src/game/galaxy/system/governor/Governor.const.ts
@@ -0,0 +1,2 @@
+export const governorSkillLevelDefault = 2;
+export const governorSkillLevelMaximum = 25;
--- src/game/galaxy/system/governor/Governor.types.ts
+++ src/game/galaxy/system/governor/Governor.types.ts
@@ -1,3 +1,9 @@
 export type TGovernor = {
   id: string;
+  avatar: string;
+  construction: number;
+  espionage: number;
+  fleet: number;
+  population: number;
+  research: number;
 };
